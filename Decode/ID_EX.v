449cc8652d7bc59713b8672515a9b205d96dbf2d
nagomez7190 <143862423+nagomez7190@users.noreply.github.com>
2024-12-13 03:17:04 -0800

    Create ID_EX.v

---
+++ Decode/ID_EX.v
@@ -0,0 +1,15 @@
+`timescale 1ns / 1ps
+
+module ID_EX (
+    input clk,
+    input reset,
+    input [143:0] bundled_in,  // Combined control signals and data inputs
+    output reg [143:0] bundled_out // Combined control signals and data outputs
+);
+    always @(posedge clk or posedge reset) begin
+        if (reset)
+            bundled_out <= 144'b0;
+        else
+            bundled_out <= bundled_in;
+    end
+endmodule
