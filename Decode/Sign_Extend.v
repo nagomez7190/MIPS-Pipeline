47982576b4caa20f5895bd6a18dfbe05d47209d1
nagomez7190 <143862423+nagomez7190@users.noreply.github.com>
2024-12-13 03:13:43 -0800

    Create Sign_Extend.v

---
+++ Decode/Sign_Extend.v
@@ -0,0 +1,8 @@
+`timescale 1ns / 1ps
+
+module Sign_Extend (
+    input [15:0] imm,
+    output [31:0] sign_ext
+);
+    assign sign_ext = {{16{imm[15]}}, imm}; // Sign-extend 16 bits to 32 bits
+endmodule
